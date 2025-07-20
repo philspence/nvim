742bf191fe62fc386cac650326e1108ad45c8d3e
Phil Spence <philspence91@gmail.com>
2025-07-20 20:08:22 +0100

    moved to using astronvim config

---
+++ lua/community.lua
@@ -0,0 +1,24 @@
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
+
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.lua" },
+  -- import/override with your plugins folder
+  { import = "astrocommunity.pack.cpp" },
+  { import = "astrocommunity.pack.cmake" },
+  { import = "astrocommunity.pack.docker" },
+  { import = "astrocommunity.pack.yaml" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.python-ruff" },
+  -- completion 
+  { import = "astrocommunity.completion.copilot-cmp" },
+  -- colorscheme
+  { import = "astrocommunity.colorscheme.monokai-pro-nvim" },
+  { import = "astrocommunity.colorscheme.sonokai" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+}
