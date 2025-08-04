d88e7ffd7e0959f3210eb14fe802bdaf69d9b27b
Phil Spence <philspence91@gmail.com>
2025-08-04 14:17:25 +0100

    added basedpyright to config

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -37,7 +37,7 @@ return {
     },
     -- enable servers that you already have installed without mason
     servers = {
-      "pyright",
+      "basedpyright",
       "clangd",
       "tailwindcss",
       "ts_ls",
