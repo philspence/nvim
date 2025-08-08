22b6d918fb9b9eb6707aecb99d862f9dff386dcb
Phil Spence <philspence91@gmail.com>
2025-08-08 11:09:56 +0100

    simplified venv discovery

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -14,8 +14,7 @@ return {
       name = "Launch file",
       program = "${file}",
       pythonPath = function()
-        local cwd = vim.fn.getcwd()
-        local venv_path = cwd .. "/.venv/bin/python"
+        local venv_path = vim.fn.getcwd() .. "/.venv/bin/python"
         if vim.fn.executable(venv_path) == 1 then
           return venv_path
         else
